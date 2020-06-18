67ac0f406e629bbcd64f07a15e015dee4e76edc9
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-18 13:05:45 +0300

    add structural changes

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from flaskblog import db, login_manager
+from . import db, login_manager
 from flask_login import UserMixin
 
 
@@ -11,6 +11,7 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
--- config.py
+++ config.py
@@ -1,11 +1,35 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://midumbi:midumbi@localhost/book_review'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='static/profile_pics'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+     
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://midumbi:midumbi@localhost/blog_test'    
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://midumbi:midumbi@localhost/book_review'
+    
+    
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}

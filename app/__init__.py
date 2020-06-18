b8144f0f9cc6606d70abec1ce69870c838ecc557
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-18 14:18:09 +0300

    add books.csv

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 mail = Mail()
 bootstap = Bootstrap()
 login_manager = LoginManager()
@@ -17,7 +19,7 @@ photos = UploadSet('photos',IMAGES)
 
 
 
-def create_app(config_class=Config):
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
     app.config['SECRET_KEY'] = 'secret'
@@ -30,10 +32,10 @@ def create_app(config_class=Config):
     from .users.routes import users as users_blueprint
     from .posts.routes import posts as posts_blueprint
     from .main.routes import main as main_blueprint
-    from .errors.handlers import errors as errors_blueprint
+    
     app.register_blueprint(users_blueprint)
     app.register_blueprint(posts_blueprint)
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(errors_blueprint)
+    
    
     return app
